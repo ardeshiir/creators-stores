f38faf70675d64ee19dcc2a161ecc1388c2ba1e4
ardeshirlaghai <ardeshir.laghaei@snapp.doctor>
2025-04-11 15:43:05 +0330

    chore: change next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    // TODO: change this later for now using ** to accept the mock data that is set in the backend
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https", // You can specify the protocol if needed
+                hostname: "**", // This allows any hostname
+            },
+        ],
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    output: "standalone",
 };
+
 
 export default nextConfig;
