a3b24cf0b03767712393f6ec2dace2530dbfad3b
ardeshirlaghai <ardeshir.laghaei@snapp.doctor>
2025-09-28 08:50:30 +0330

    fix: add new user

--- src/app/users/new/page.tsx
+++ src/app/users/new/page.tsx
@@ -1,6 +1,6 @@
 import UserForm from '@/components/form/UserForm'
 
-const New = () => {
+const Page = () => {
   return (
     <div className="container mx-auto flex flex-col items-center px-[24px] md:px-[56px] lg:px-[80px]">
       <UserForm />
@@ -8,4 +8,4 @@ const New = () => {
   )
 }
 
-export default New
+export default Page
